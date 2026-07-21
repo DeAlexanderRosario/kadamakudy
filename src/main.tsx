14aecff2d98cc53660602a8df465c34731fa097c
ecoscrap-1011 <ecoscrap.1011@gmail.com>
2026-07-21 12:00:39 +0530

    hi

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Initialize Google Analytics if VITE_GA_ID environment variable is provided
-const gaId = import.meta.env.VITE_GA_ID;
-if (gaId) {
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
-  document.head.appendChild(script);
-
-  const scriptInit = document.createElement('script');
-  scriptInit.innerHTML = `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${gaId}');
-  `;
-  document.head.appendChild(scriptInit);
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
